0221767200ec3b98e828f64d55bb5a9b870aacbc
akthrmsx <akt.hrms@gmail.com>
2024-10-29 03:59:23 +0900

    update

--- action.go
+++ action.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -22,4 +24,67 @@ func filterOut(path string, extension string, minSize int64, info os.FileInfo) b
 func listFile(path string, out io.Writer) error {
 	_, err := fmt.Fprintln(out, path)
 	return err
+}
+
+func deleteFile(path string, logger *log.Logger) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+
+	logger.Panicln(path)
+	return nil
+}
+
+func archiveFile(destDir string, root string, path string) error {
+	info, err := os.Stat(destDir)
+
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("%s is not directory", destDir)
+	}
+
+	relDir, err := filepath.Rel(root, filepath.Dir(path))
+
+	if err != nil {
+		return err
+	}
+
+	dest := fmt.Sprintf("%s.gz", filepath.Base(path))
+	target := filepath.Join(destDir, relDir, dest)
+
+	if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+		return err
+	}
+
+	out, err := os.OpenFile(target, os.O_RDWR|os.O_CREATE, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	in, err := os.Open(path)
+
+	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	writer := gzip.NewWriter(out)
+	writer.Name = filepath.Base(path)
+
+	if _, err = io.Copy(writer, in); err != nil {
+		return err
+	}
+
+	if err = writer.Close(); err != nil {
+		return err
+	}
+
+	return out.Close()
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -13,6 +14,9 @@ type config struct {
 	extension string
 	size      int64
 	list      bool
+	delete    bool
+	writer    io.Writer
+	archive   string
 }
 
 func main() {
@@ -20,12 +24,34 @@ func main() {
 	extension := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 	list := flag.Bool("list", false, "List files only")
+	delete := flag.Bool("delete", false, "Delete files")
+	filename := flag.String("log", "", "Log deletes to this file")
+	archive := flag.String("archive", "", "Archive files")
 	flag.Parse()
+
+	var (
+		writer = os.Stdout
+		err    error
+	)
+
+	if *filename != "" {
+		writer, err = os.OpenFile(*filename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		defer writer.Close()
+	}
 
 	config := config{
 		extension: *extension,
 		size:      *size,
 		list:      *list,
+		delete:    *delete,
+		writer:    writer,
+		archive:   *archive,
 	}
 
 	if err := run(*root, os.Stdout, config); err != nil {
@@ -35,6 +61,8 @@ func main() {
 }
 
 func run(root string, out io.Writer, config config) error {
+	logger := log.New(config.writer, "DELETE FILE: ", log.LstdFlags)
+
 	return filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -46,6 +74,16 @@ func run(root string, out io.Writer, config config) error {
 
 		if config.list {
 			return listFile(path, out)
+		}
+
+		if config.archive != "" {
+			if err := archiveFile(config.archive, root, path); err != nil {
+				return err
+			}
+		}
+
+		if config.delete {
+			return deleteFile(path, logger)
 		}
 
 		return listFile(path, out)
